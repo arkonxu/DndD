dc0f7f529f06cb5dd71d4883256472dd97ed70ee
arkonxu <carlos.arkon@gmail.com>
2019-06-13 17:29:26 +0200

    Actualizacion 7

--- app/src/main/java/com/example/dd/Classes/AdapterViewHolderClasses.kt
+++ app/src/main/java/com/example/dd/Classes/AdapterViewHolderClasses.kt
@@ -2,6 +2,7 @@ package com.example.dd.Classes
 
 import android.app.Activity
 import android.content.Intent
+import android.os.StrictMode
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -9,8 +10,10 @@ import android.view.ViewGroup
 import com.example.dd.R
 import com.example.dd.com.example.dd.Classes.ClassesDetails
 import com.example.dd.setDrawable
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.recyclerview_adapter.view.*
 import java.io.Serializable
+import java.net.URL
 
 class Adapter  (val classesList: List<Result>, var actividad: Activity) : RecyclerView.Adapter<Adapter.ViewHolder>() {
 
@@ -32,15 +35,31 @@ class Adapter  (val classesList: List<Result>, var actividad: Activity) : Recycl
         return view
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int){holder.rellenar(classesList[position])}
+    override fun onBindViewHolder(holder: ViewHolder, position: Int){holder.rellenar(classesList[position], position)
+
+    }
 
     override fun getItemCount():Int{return classesList.size}
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun rellenar(item : Result) {
+
+        fun rellenar(item : Result, position: Int) {
             itemView.nc.text=item.name
+            itemView.descripcion.text= "Hit Points: "+apiHP(item).toString()
             itemView.imagenClase.clipToOutline=true
             itemView.imagenClase.setDrawable(item.name.decapitalize())
+
+        }
+
+        private fun apiHP(item : Result): Int {
+
+            val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+            val builder = GsonBuilder()
+            val gson = builder.create()
+            val json = URL(item.url).readText()
+            var hP = gson.fromJson(json, Class::class.java)
+            return hP.hit_die
 
         }
     }
